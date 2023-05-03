96f3d1c8af60e8a25c8464db4b60e3b33e8accba
Haim Ashkenazi <haim@babysnakes.org>
2023-05-03 22:28:26 +0300

    Small fixes:
      - Remove deleted links
      - First file (just to verify that top level docs are listed in the menu)

---
+++ docs/overview.md
@@ -0,0 +1,9 @@
+---
+sidebar_label: 'Overview'
+sidebar_position: 1
+---
+
+# Some Overview
+
+This is a short overview of _DynamicEnvironment_.
+
--- docusaurus.config.js
+++ docusaurus.config.js
@@ -91,8 +91,8 @@ const config = {
             title: 'Docs',
             items: [
               {
-                label: 'Tutorial',
-                to: '/docs/intro',
+                label: 'Documentation',
+                to: '/docs/overview',
               },
             ],
           },
@@ -116,10 +116,10 @@ const config = {
           {
             title: 'More',
             items: [
-              {
-                label: 'Blog',
-                to: '/blog',
-              },
+              // {
+              //   label: 'Blog',
+              //   to: '/blog',
+              // },
               {
                 label: 'GitHub',
                 href: 'https://github.com/facebook/docusaurus',
